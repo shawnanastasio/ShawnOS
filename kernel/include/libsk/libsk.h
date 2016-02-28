89bfae655d03603a5c42e08ce24061e3dffed140
Shawn Anastasio <shawnanastasio@yahoo.com>
2016-02-28 11:22:19 -0600

    All all basic freestanding libc functions into libsk that are required by gcc

--- kernel/include/libsk/libsk.h
+++ kernel/include/libsk/libsk.h
@@ -7,6 +7,12 @@
 
 size_t strlen(const char* str);
 
+int memcmp(const void* s1, const void* s2,size_t n);
+
 void *memcpy(void *str1, const void *str2, size_t n);
+
+void *memmove(void *dest, const void *src, size_t n);
+
+void *memset(void *s, int c, size_t n);
 
 #endif
