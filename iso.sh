03bffdbcdd471944b9139c27f170405a40a64a7d
Shawn Anastasio <shawnanastasio@yahoo.com>
2016-03-02 13:03:12 -0600

    Add grub-mkrescue checks in iso.sh

--- iso.sh
+++ iso.sh
@@ -6,12 +6,22 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
+# Check to make sure we have grub(2)-mkrescue
+if type grub-mkrescue >/dev/null; then
+    MKRESCUE="grub-mkrescue"
+elif type grub2-mkrescue >/dev/null; then
+    MKRESCUE="grub2-mkrescue"
+else
+    echo "Please ensure grub-mkrescue or grub2-mkrescue is installed and in your path"
+    exit
+fi
+
 cp sysroot/boot/shawnos.kernel isodir/boot/shawnos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "shawnos" {
 	multiboot /boot/shawnos.kernel
 }
 EOF
-grub2-mkrescue -o shawnos.iso isodir
+$MKRESCUE -o shawnos.iso isodir
 
 
