34147deffabb0e48a0e50504e92cd34803c219e7
Shawn Anastasio <shawnanastasio@yahoo.com>
2016-04-04 20:09:40 -0500

    libsc: Add inportb function

---
+++ libsc/arch/i386/inportb.c
@@ -0,0 +1,6 @@
+unsigned char inportb (unsigned short _port)
+{
+    unsigned char rv;
+    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
+}
--- libsc/include/pc.h
+++ libsc/include/pc.h
@@ -1,1 +1,2 @@
 void outportb (unsigned short _port, unsigned char _data);
+unsigned char inportb (unsigned short _port);
