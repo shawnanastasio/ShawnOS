981be7975abf0abbb93673bca8a670f0d6efea92
Shawn Anastasio <shawnanastasio@yahoo.com>
2016-04-04 21:04:17 -0500

    Add PS/2 Keyboard Drivers

---
+++ kernel/drivers/pc/pckbd.c
@@ -0,0 +1,48 @@
+/**
+ * Driver for standard PC PS/2 Keyboards
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pc.h>
+
+#include <kernel/kernel.h>
+#include <drivers/pc/pckbd.h>
+#include <arch/i386/isr.h>
+#include <arch/i386/irq.h>
+
+unsigned char *pckbd_selected_scancode_table;
+
+void pckbd_irq_input_handler(struct regs *r) {
+    if ((r->int_no-32) != 1) {
+        printf("ERROR: this routine needs to be triggered from IRQ 1\n");
+        abort();
+    }
+
+    unsigned char cur_scancode;
+
+    // Read from PS/2 buffer
+    cur_scancode = inportb(0x60);
+
+    if (cur_scancode & 0x80) {
+        //Key has just been released
+
+    } else {
+        //Look up char in scancode table
+        unsigned char cur_char = pckbd_selected_scancode_table[cur_scancode];
+
+        //For now, just print out debug to screen
+        char temp[2];
+        temp[0] = (char)cur_char;
+        temp[1] = '\0';
+        printk_debug(strcat("KEY PRESSED: ", temp));
+    }
+}
+
+// Install pckbd handler with specificed scancode table to IRQ1
+void pckbd_install_irq(unsigned char *scancode_table) {
+    pckbd_selected_scancode_table = scancode_table;
+    irq_install_handler(1, pckbd_irq_input_handler);
+}
---
+++ kernel/include/drivers/pc/pckbd.h
@@ -0,0 +1,48 @@
+#pragma once
+
+
+void pckbd_install_irq(unsigned char *scancode_table);
+
+/**
+ * Define scancode tables for different PS/2 keyboards
+ */
+
+// US QWERTY
+unsigned char pckbd_us_qwerty_sc[128] = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',
+    '9', '0', '-', '=', '\b',
+    '\t',
+    'q', 'w', 'e', 'r',
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+    0,
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+    '\'', '`',   0,
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n',
+    'm', ',', '.', '/',   0,
+    '*',
+    0,	// Alt
+    ' ',	// Space bar
+    0,	// Caps lock
+    0,	// 59 - F1 key ... >
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	// < ... F10
+    0,	// 69 - Num lock
+    0,	// Scroll Lock
+    0,	// Home key
+    0,	// Up Arrow
+    0,	// Page Up
+    '-',
+    0,	// Left Arrow
+    0,
+    0,	// Right Arrow
+    '+',
+    0,	// 79 - End key
+    0,	// Down Arrow
+    0,	// Page Down
+    0,	// Insert Key
+    0,	// Delete Key
+    0,   0,   0,
+    0,	// F11 Key
+    0,	// F12 Key
+    0,	// All other keys are undefined
+};
