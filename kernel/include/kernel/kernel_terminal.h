c99f31af174bc28f9aa1a62e7603e15712d0bcd9
Shawn Anastasio <shawnanastasio@yahoo.com>
2016-04-20 13:17:05 -0500

    Add backspace support in kernel_terminal.c

--- kernel/include/kernel/kernel_terminal.h
+++ kernel/include/kernel/kernel_terminal.h
@@ -10,4 +10,5 @@ void kernel_terminal_update_tick();
 void kernel_terminal_handle_escapecode(char c);
 bool kernel_terminal_check_escapecode(char c);
 void kernel_terminal_handle_overflow();
+void kernel_terminal_backspace();
 void kernel_terminal_putentry(char c, uint8_t color, size_t x, size_t y);
--- kernel/kernel/kernel_terminal.c
+++ kernel/kernel/kernel_terminal.c
@@ -15,7 +15,7 @@
 char kernel_terminal_escapecodes[15] =
 {
     '\n',
-    0,
+    '\b',
     0,
     0,
     0,
@@ -39,63 +39,63 @@ uint8_t color;
 uint16_t terminal_buffer[VGA_HEIGHT * VGA_WIDTH];
 
 void kernel_terminal_init(uint16_t refresh_rate) {
-  // Initalize driver for device to output to
-  vga_textmode_initialize();
-  color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+    // Initalize driver for device to output to
+    vga_textmode_initialize();
+    color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
 
-  // Initialize empty terminal_buffer
-  for (size_t y = 0; y < VGA_HEIGHT; y++) {
-    for (size_t x = 0; x < VGA_WIDTH; x++) {
-      const size_t index = y * VGA_WIDTH + x;
-      terminal_buffer[index] = make_vgaentry(' ', color);
+    // Initialize empty terminal_buffer
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t index = y * VGA_WIDTH + x;
+            terminal_buffer[index] = make_vgaentry(' ', color);
+        }
     }
-  }
 
-  // Install terminal update tick to timer
-  // TODO: un-hardcode use of PIT
-  struct pit_routine kernel_terminal_task = {
-      10000/refresh_rate,
-      kernel_terminal_update_tick
-  };
-  pit_install_scheduler_routine(kernel_terminal_task);
+    // Install terminal update tick to timer
+    // TODO: un-hardcode use of PIT
+    struct pit_routine kernel_terminal_task = {
+        10000/refresh_rate,
+        kernel_terminal_update_tick
+    };
+    pit_install_scheduler_routine(kernel_terminal_task);
 
-  // Initially update terminal
-  kernel_terminal_update_tick();
+    // Initially update terminal
+    kernel_terminal_update_tick();
 }
 
 void kernel_terminal_update_tick() {
-  uint16_t i;
+    uint16_t i;
 
-  // Get kernel STDOUT buffer
-  char stdout_buffer[STDOUT_MAX_BUFFER];
-  uint16_t stdout_buffer_length = kernel_buffer_stdout_get(stdout_buffer);
+    // Get kernel STDOUT buffer
+    char stdout_buffer[STDOUT_MAX_BUFFER];
+    uint16_t stdout_buffer_length = kernel_buffer_stdout_get(stdout_buffer);
 
-  if(stdout_buffer_length != 0) {
-    // Refresh current console
-    vga_textmode_clear();
+    if(stdout_buffer_length != 0) {
+        // Refresh current console
+        vga_textmode_clear();
 
-    // Restore old text from our buffer
-    vga_textmode_writebuffer(terminal_buffer, VGA_WIDTH*VGA_HEIGHT);
+        // Restore old text from our buffer
+        vga_textmode_writebuffer(terminal_buffer, VGA_WIDTH*VGA_HEIGHT);
 
-    for(i=0; i<stdout_buffer_length; i++) {
-      char cur_char = stdout_buffer[i];
+        for(i=0; i<stdout_buffer_length; i++) {
+            char cur_char = stdout_buffer[i];
 
-      // Parse char for terminal escape codes
-      if (kernel_terminal_check_escapecode(cur_char)) {
-        kernel_terminal_handle_escapecode(cur_char);
-      } else { // No escape codes
+            // Parse char for terminal escape codes
+            if (kernel_terminal_check_escapecode(cur_char)) {
+                kernel_terminal_handle_escapecode(cur_char);
+            } else { // No escape codes
 
-        // Check for overflow and wrap text accordingly
-        kernel_terminal_handle_overflow();
+                // Check for overflow and wrap text accordingly
+                kernel_terminal_handle_overflow();
 
-        // Check for need to scroll and handle
-        // kernel_terminal_handle_scroll();
+                // Check for need to scroll and handle
+                // kernel_terminal_handle_scroll();
 
-        kernel_terminal_putentry(cur_char, color, cur_xpos++, cur_ypos);
-      }
+                kernel_terminal_putentry(cur_char, color, cur_xpos++, cur_ypos);
+            }
+        }
+
     }
-
-  }
 }
 
 /**
@@ -103,14 +103,14 @@ void kernel_terminal_update_tick() {
  * @param c char to test
  */
 bool kernel_terminal_check_escapecode(char c) {
-  size_t i;
-  for (i=0; i<ESCAPECODE_LENGTH; i++) {
-    if (c == kernel_terminal_escapecodes[i]) {
-      return true;
+    size_t i;
+    for (i=0; i<ESCAPECODE_LENGTH; i++) {
+        if (c == kernel_terminal_escapecodes[i]) {
+            return true;
+        }
     }
-  }
 
-  return false;
+    return false;
 }
 
 /**
@@ -118,21 +118,37 @@ bool kernel_terminal_check_escapecode(char c) {
  * @param c [description]
  */
 void kernel_terminal_handle_escapecode(char c) {
-  //TODO: Insert logic to handle all escape codes
+    //TODO: Insert logic to handle all escape codes
 
-  switch(c) {
-    case '\n':
-      cur_ypos++;
-      cur_xpos = 0;
-      break;
-  }
+    switch(c) {
+        case '\n':
+            cur_ypos++;
+            cur_xpos = 0;
+            break;
+        case '\b':
+            kernel_terminal_backspace();
+            break;
+    }
 }
 
 void kernel_terminal_handle_overflow() {
-  if (cur_xpos >= VGA_WIDTH) {
-    cur_xpos = 0;
-    cur_ypos++;
-  }
+    if (cur_xpos >= VGA_WIDTH) {
+        cur_xpos = 0;
+        cur_ypos++;
+    }
+}
+
+/**
+ * Removes the last printed character in display and terminal buffer
+ */
+void kernel_terminal_backspace() {
+    if (cur_xpos == 0 && cur_ypos == 0) {
+        kernel_terminal_putentry(' ', color, 0, 0);
+    } else if (cur_xpos == 0) {
+        kernel_terminal_putentry(' ', color, VGA_WIDTH, cur_ypos-1);
+    } else {
+        kernel_terminal_putentry(' ', color, cur_xpos-1, cur_ypos);
+    }
 }
 
 /**
@@ -141,11 +157,11 @@ void kernel_terminal_handle_overflow() {
  * @param color color of char to put
  * @param x     x coordinate
  * @param y     y coordinate
- */
+*/
 void kernel_terminal_putentry(char c, uint8_t color, size_t x, size_t y) {
-  // Update actual VGA output
-  vga_textmode_putentryat(c, color, x, y);
+    // Update actual VGA output
+    vga_textmode_putentryat(c, color, x, y);
 
-  // Update local buffer
-  terminal_buffer[y * VGA_WIDTH + x] = make_vgaentry(c, color);
+    // Update local buffer
+    terminal_buffer[y * VGA_WIDTH + x] = make_vgaentry(c, color);
 }
